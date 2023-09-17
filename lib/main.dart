65e8fa9cf485de1f67552a33064ec5514b1bff10
Joshuaj0209 <joshuaj0209@gmail.com>
2023-09-17 23:12:55 +0200

    Connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:e_learning_app/firebase_options.dart';
 import 'package:e_learning_app/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
