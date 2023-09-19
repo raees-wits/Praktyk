fc0b7fa10fee9c065bad52cc0560713c885552cf
Joshuaj0209 <joshuaj0209@gmail.com>
2023-09-19 23:02:12 +0200

    Fix for daily goals not displaying

--- lib/screens/home_screen_content.dart
+++ lib/screens/home_screen_content.dart
@@ -23,118 +23,140 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
       showGoalsOverlay = !showGoalsOverlay;
     });
   }
+  void _closeGoalsOverlay() {
+    setState(() {
+      showGoalsOverlay = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView(
+      child: Stack(
         children: [
-          CustomeAppBar(),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Column(
-              children: [
-                Row(
+          ListView(
+            children: [
+              CustomeAppBar(),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Hi PraiseGod",
+                    Row(
+                      children: [
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Hi PraiseGod",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              "Vandag is 'n goeie dag om \n iets nuuts te leer!",
+                              style: TextStyle(
+                                color: Colors.black54,
+                                wordSpacing: 2.5,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        Column(
+                          children: [
+                            Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                color: kpurple,
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Image.asset(
+                                "assets/images/profile.png",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //sorting
+                    Sorting(
+                      showGoalsOverlay: showGoalsOverlay,
+                      toggleGoalsOverlay: _toggleGoalsOverlay,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //category list
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Categories",
                           style: TextStyle(
-                            fontSize: 28,
+                            fontSize: 18,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          "Vandag is 'n goeie dag om \n iets nuuts te leer!",
-                          style: TextStyle(
-                            color: Colors.black54,
-                            wordSpacing: 2.5,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+                        InkWell(
+                          onTap: () {},
+                          child: const Text(
+                            "See All",
+                            style: TextStyle(fontSize: 16, color: kblue),
                           ),
                         ),
                       ],
                     ),
-                    const Spacer(),
-                    Column(
-                      children: [
-                        Container(
-                          height: 70,
-                          width: 70,
-                          decoration: BoxDecoration(
-                              color: kpurple,
-                              borderRadius: BorderRadius.circular(15.0)),
-                          child: Image.asset(
-                            "assets/images/profile.png",
-                          ),
-                        ),
-                      ],
+
+                    //now we create model of our images and colors which we will use in our app
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //we can not use gridview inside column
+                    //use shrinkwrap and physical scroll
+                    CategoryList(),
+                    const SizedBox(
+                      height: 20,
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                //sorting
-                Sorting(
-                  showGoalsOverlay: showGoalsOverlay,
-                  toggleGoalsOverlay: _toggleGoalsOverlay,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                //category list
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Categories",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+              ),
+            ],
+          ),
+          // Show the overlay when showGoalsOverlay is true
+          // Show the overlay when showGoalsOverlay is true
+          if (showGoalsOverlay)
+            Container(
+              color: Colors.black.withOpacity(0.5), // Semi-transparent black background
+              child: Center(
+                child: FractionallySizedBox(
+                  widthFactor: 0.9, // Adjust the width as needed
+                  heightFactor: 0.9, // Adjust the height as needed
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0), // Adjust the radius as needed
+                    child: Container(
+                      color: Colors.white, // Background color of the overlay
+                      child: GoalsOverlayWidget(
+                        onClose: _closeGoalsOverlay, // Pass the close callback
                       ),
-                    ),
-                    InkWell(
-                      onTap: () {},
-                      child: const Text(
-                        "See All",
-                        style: TextStyle(fontSize: 16, color: kblue),
-                      ),
-                    ),
-                  ],
-                ),
-
-                // Show the overlay when showGoalsOverlay is true
-                if (showGoalsOverlay)
-                  Container(
-                    color: Colors.black.withOpacity(0.5), // Semi-transparent black background
-                    child: const Center(
-                      child: GoalsOverlayWidget(), // Use GoalsOverlayWidget
                     ),
                   ),
-
-                //now we create model of our images and colors which we will use in our app
-                const SizedBox(
-                  height: 20,
                 ),
-                  //we can not use gridview inside column
-                  //use shrinkwrap and physical scroll
-                CategoryList(),
-                const SizedBox(
-                  height: 20,
-                ),
-              ],
+              ),
             ),
-          )
         ],
       ),
     );
