12ac490665833218055c0b902020cfa85c6d4599
neo-nkosi <2437872@students.wits.ac.za>
2023-09-25 16:27:06 +0200

    Updated sort answer questions

--- lib/screens/activity_short_answer.dart
+++ lib/screens/activity_short_answer.dart
@@ -39,7 +39,7 @@ class _ShortAnswerQuestionsState extends State<ShortAnswerQuestions> {
         feedback = "Correct!";
         isCorrect = true;
       } else {
-        feedback = "Incorrect! The correct answer is $correctAnswer";
+        feedback = "Incorrect! The correct answer is:\n\n $correctAnswer";
         isCorrect = false;
       }
       opacityLevel = 1.0; // Set opacity level to 1 to make the icon visible
@@ -56,14 +56,14 @@ class _ShortAnswerQuestionsState extends State<ShortAnswerQuestions> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xFF9ba0fc),
         elevation: 0.0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black), // Set the arrow icon color
-          onPressed: () => Navigator.of(context).pop(), // Pop the current screen
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         title: Text(
-          "Pick a choice",
+          "Short Answer Questions",
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.bold,
@@ -82,134 +82,154 @@ class _ShortAnswerQuestionsState extends State<ShortAnswerQuestions> {
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [
-                Color(0x669ba0fc),
-                Color(0x999ba0fc),
-                Color(0xcc9ba0fc),
                 Color(0xFF9ba0fc),
+                Color(0xcc9ba0fc),
+                Color(0x999ba0fc),
+                Color(0x669ba0fc),
               ],
             ),
           ),
           child: Padding(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 70),
-                Text(
-                  "Question ${currentQuestionIndex + 1}",
-                  style: TextStyle(
-                    fontFamily: 'Cursive',
-                    fontSize: 45,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 40),
-                Text(
-                  questions[currentQuestionIndex],
-                  style: TextStyle(
-                    fontFamily: 'NunitoSans',
-                    fontSize: 32,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 40),
-                TextField(
-                  controller: answerController,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: "Your answer here",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                ElevatedButton(
-                  onPressed: submitAnswer,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: EdgeInsets.all(15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
-                  child: Text(
-                    "Submit",
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 70),
+                  Text(
+                    "Question ${currentQuestionIndex + 1}",
                     style: TextStyle(
-                      color: Color(0xFF9ba0fc),
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Cursive',
+                      fontSize: 45,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Text(
+                      questions[currentQuestionIndex],
+                      style: TextStyle(
+                        fontFamily: 'NunitoSans',
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
-                if (feedback != null)
-                  Column(
-                    children: [
-                      SizedBox(height: 20),
-                      Text(
-                        feedback!,
-                        style: TextStyle(
-                          color: isCorrect! ? Colors.green : Colors.red,
-                          fontSize: 18,
-                        ),
+                  SizedBox(height: 40),
+                  TextField(
+                    controller: answerController,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Your answer here",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                      SizedBox(height: 20),
-                      AnimatedOpacity(
-                        opacity: opacityLevel,
-                        duration: Duration(seconds: 1),
-                        child: Icon(
-                          isCorrect! ? Icons.check : Icons.close,
-                          color: isCorrect! ? Colors.green : Colors.red,
-                          size: 30,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-              ],
+                  SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: submitAnswer,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Text(
+                      "Submit",
+                      style: TextStyle(
+                        color: Color(0xFF9ba0fc),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  if (feedback != null)
+                    Column(
+                      children: [
+                        SizedBox(height: 20),
+                        Text(
+                          feedback!,
+                          style: TextStyle(
+                            color: isCorrect! ? Colors.green : Colors.red,
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        AnimatedOpacity(
+                          opacity: opacityLevel,
+                          duration: Duration(seconds: 1),
+                          child: Icon(
+                            isCorrect! ? Icons.check : Icons.close,
+                            color: isCorrect! ? Colors.green : Colors.red,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
           ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
+        color: Color(0x999ba0fc),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  if (currentQuestionIndex > 0) {
-                    setState(() {
-                      currentQuestionIndex--;
-                    });
-                  }
-                },
-                child: Text("Prev"),
-              ),
-              Text(
-                "Question ${currentQuestionIndex + 1}",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (currentQuestionIndex > 0) {
+                      setState(() {
+                        currentQuestionIndex--;
+                        feedback = null; // Clear the feedback
+                        isCorrect = null; // Clear the correctness status
+                        answerController.clear(); // Clear the TextField content
+                      });
+                    }
+                  },
+                  child: Text("Prev"),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (currentQuestionIndex < questions.length - 1) {
-                    setState(() {
-                      currentQuestionIndex++;
-                    });
-                  }
-                },
-                child: Text("Next"),
-              ),
-            ],
+                Text(
+                  "Question ${currentQuestionIndex + 1}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (currentQuestionIndex < questions.length - 1) {
+                      setState(() {
+                        currentQuestionIndex++;
+                        feedback = null; // Clear the feedback
+                        isCorrect = null; // Clear the correctness status
+                        answerController.clear(); // Clear the TextField content
+                      });
+                    }
+                  },
+                  child: Text("Next"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
