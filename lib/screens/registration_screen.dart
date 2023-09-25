716ba390fd0c75d13c0ca6eeb84ffcec5e9edf94
neo-nkosi <2437872@students.wits.ac.za>
2023-09-25 22:36:01 +0200

    updated registration page to include password

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -8,6 +8,7 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
   String? registrationType;
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
@@ -17,7 +18,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController gradeController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +31,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                //BACKGROUND COLOR
-                // after the 0x part, the opacity is controlled by
-                //the following 2 characters
-                //***this creates the colour gradient in the bg***
-                // use color-hex.com for color selections
                 colors: [
                   Color(0x66ff6374),
                   Color(0x99ff6374),
@@ -47,89 +42,132 @@ class _SignUpScreenState extends State<SignUpScreen> {
           child: SingleChildScrollView(
             physics: AlwaysScrollableScrollPhysics(),
             padding: EdgeInsets.symmetric(horizontal: 25, vertical: 120),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-              ),
-              SizedBox(height: 50),DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
-                    labelText: 'Registration Type',
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  hint: Text("Please select registration type"),
-                  value: registrationType,
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("Student"),
-                      value: "Student",
+                  SizedBox(height: 50),
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: 'Registration Type',
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                    DropdownMenuItem(
-                      child: Text("Teacher"),
-                      value: "Teacher",
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Parent"),
-                      value: "Parent",
-                    ),
+                    hint: Text("Please select registration type"),
+                    value: registrationType,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Student"),
+                        value: "Student",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Teacher"),
+                        value: "Teacher",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Parent"),
+                        value: "Parent",
+                      ),
+                    ],
+                    validator: (value) =>
+                    value == null
+                        ? 'Please select a registration type'
+                        : null,
+                    onChanged: (String? value) {
+                      setState(() {
+                        registrationType = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  buildTextField("First Name", firstNameController, true),
+                  SizedBox(height: 20),
+                  buildTextField("Last Name", lastNameController, true),
+                  SizedBox(height: 20),
+                  buildTextField("Email Address", emailController, true,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an Email Address';
+                        }
+                        // Add regex for email validation
+                        final regexEmail = RegExp(
+                            r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                        if (!regexEmail.hasMatch(value)) {
+                          return 'Enter a valid Email Address';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 20),
+                  buildTextField("Phone Number", phoneController, true,
+                      keyboardType: TextInputType.phone, validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a phone number';
+                        }
+                        if (value.length != 10) {
+                          return 'Phone number must be 10 digits long';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 20),
+                  buildTextField(
+                      "Password", passwordController, true, isPassword: true),
+                  SizedBox(height: 20),
+                  buildTextField(
+                      "Confirm Password", confirmPasswordController, true,
+                      isPassword: true, validator: (value) {
+                    if (value != passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  }),
+                  if (registrationType == "Student") ...[
+                    SizedBox(height: 20),
+                    buildTextField("School", schoolController, true),
+                    SizedBox(height: 20),
+                    buildTextField("Grade", gradeController, true),
+                    SizedBox(height: 20),
                   ],
-                  onChanged: (String? value) {
-                    setState(() {
-                      registrationType = value;
-                    });
-                  },
-                ),
-                SizedBox(height: 20),
-                buildTextField("First Name", firstNameController),
-              SizedBox(height: 20),
-              buildTextField("Last Name", lastNameController),
-              SizedBox(height: 20),
-              buildTextField("Email Address", emailController),
-              SizedBox(height: 20),
-              buildTextField("Phone Number", phoneController),
-              SizedBox(height: 20),
-              // If a student registers, then
-              if (registrationType == "Student") ...[
-                buildTextField("School", schoolController),
-                SizedBox(height: 20),
-                buildTextField("Grade", gradeController),
-                SizedBox(height: 20),
-              ],
-                buildPasswordField("Password", passwordController),
-                SizedBox(height: 20),
-                buildPasswordField("Confirm Password", confirmPasswordController),
-                SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  print("Sign Up Pressed");
-                },
-                child: Text("Sign Up",
-                style: TextStyle(
-                  color: Colors.black,
-                )),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  padding: EdgeInsets.all(15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        print("Form is valid");
+                        // Process data
+                      } else {
+                        print("Form is invalid");
+                      }
+                    },
+                    child: Text("Sign Up",
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 
   Widget buildTextField(String hint, TextEditingController controller) {
