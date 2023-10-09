43033e65ad337bd71701d39d3b54f60665604fc6
Joshuaj0209 <joshuaj0209@gmail.com>
2023-10-09 11:51:40 +0200

    Fixed community screen submission

--- lib/screens/Community_Screen.dart
+++ lib/screens/Community_Screen.dart
@@ -81,14 +81,80 @@ class _CommunityScreenState extends State<CommunityScreen> {
                     ElevatedButton(
                       onPressed: () async {
                         // Handle question submission here
-                        // ...
+                        String enteredQuestion = questionController.text;
+                        if (enteredQuestion.isNotEmpty) {
+                          setState(() {
+                            isUploading = true; // Set uploading status to true
+                            questionPosted = false; // Reset questionPosted status
+                          });
+
+                          if (imageFile != null) {
+                            final String fileName =
+                                DateTime.now().millisecondsSinceEpoch.toString() + '.jpg';
+                            final Reference storageRef =
+                            FirebaseStorage.instance.ref().child('images/$fileName');
+                            final UploadTask uploadTask = storageRef.putFile(imageFile!);
+
+                            try {
+                              // Wait for the image upload to complete
+                              final TaskSnapshot snapshot =
+                              await uploadTask.whenComplete(() => null);
+
+                              // Get the download URL of the uploaded image
+                              final String imageUrl =
+                              await snapshot.ref.getDownloadURL();
+
+                              // Now, you can save the image URL along with the question to Firestore
+                              await firestore.collection('questions').add({
+                                'text': enteredQuestion,
+                                'timestamp': FieldValue.serverTimestamp(),
+                                'imageUrl': imageUrl, // Save the image URL in Firestore
+                              });
+
+                              // Clear the text field after submitting
+                              questionController.clear();
+                              // Reload questions to reflect the newly added one
+                              loadQuestions();
+                              clearUploadStatus();
+                              setState(() {
+                                questionPosted = true; // Set questionPosted status to true
+                              });
+                            } catch (error) {
+                              print('Upload error: $error');
+                              clearUploadStatus();
+                            }
+                          } else {
+                            // If no image was selected, just save the question without an image
+                            await firestore.collection('questions').add({
+                              'text': enteredQuestion,
+                              'timestamp': FieldValue.serverTimestamp(),
+                            });
+
+                            // Clear the text field after submitting
+                            questionController.clear();
+                            // Reload questions to reflect the newly added one
+                            loadQuestions();
+                            clearUploadStatus();
+                            setState(() {
+                              questionPosted = true; // Set questionPosted status to true
+                            });
+                          }
+                        }
                       },
                       child: Text('Submit'),
                     ),
                     ElevatedButton(
                       onPressed: () async {
-                        // Handle image attachment here
-                        // ...
+                        final imagePicker = ImagePicker();
+                        final pickedImageFile =
+                        await imagePicker.pickImage(source: ImageSource.gallery);
+
+                        // Set the imageFile variable to the pickedImageFile
+                        if (pickedImageFile != null) {
+                          setState(() {
+                            imageFile = File(pickedImageFile.path);
+                          });
+                        }
                       },
                       child: Text('Attach Image'),
                     ),
