22d0f1118a173d831bfcb27ac9f1f5c0fe38456f
Joshuaj0209 <127685979+Joshuaj0209@users.noreply.github.com>
2023-10-17 18:31:17 +0200

    Add new questions to MTC

--- lib/screens/TeacherScreens/teacher_match_the_column_screen.dart
+++ lib/screens/TeacherScreens/teacher_match_the_column_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TeacherMatchTheColumn extends StatefulWidget {
   final String updateMode;
@@ -11,10 +12,40 @@ class TeacherMatchTheColumn extends StatefulWidget {
 }
 
 class _TeacherMatchTheColumnState extends State<TeacherMatchTheColumn> {
-  String dropdownValue = 'Category 1'; // Default value for the dropdown
+  String? dropdownValue;
   final questionController = TextEditingController();
   final answerController = TextEditingController();
+  final List<String> categories = [];
+  bool isLoading = true; // Flag to check if data is still loading
 
+  @override
+  void initState() {
+    super.initState();
+    fetchCategories(); // Fetch the categories when the widget is initialized
+  }
+
+  Future<void> fetchCategories() async {
+    // Fetch the category names from the 'Match The Column' collection
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('Match The Column').get();
+    final allData = querySnapshot.docs.map((doc) => doc.id).toList(); // Get document IDs as category names
+
+    // Update the state with the fetched category names and assign the default dropdown value
+    setState(() {
+      categories.addAll(allData);
+      dropdownValue = categories.isNotEmpty ? categories[0] : null;
+      isLoading = false; // Set isLoading to false once data is fetched
+    });
+  }
+
+  Future<void> submitQuestion() async {
+    // Add a new document inside the category with the question and answer
+    if (dropdownValue != null) {
+      await FirebaseFirestore.instance.collection('Match The Column').doc(dropdownValue).collection('items').add({
+        'question': questionController.text,
+        'answer': answerController.text,
+      });
+    }
+  }
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
@@ -29,7 +60,9 @@ class _TeacherMatchTheColumnState extends State<TeacherMatchTheColumn> {
       appBar: AppBar(
         title: const Text('Teacher Match The Column'),
       ),
-      body: Padding(
+      body: isLoading // Check if data is still loading
+          ? Center(child: CircularProgressIndicator()) // Show loading indicator if yes
+          : Padding( // Otherwise, show the regular UI
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -43,7 +76,7 @@ class _TeacherMatchTheColumnState extends State<TeacherMatchTheColumn> {
                     dropdownValue = newValue!;
                   });
                 },
-                items: <String>['Category 1', 'Category 2', 'Category 3', 'Category 4']
+                items: categories // Use categories list here, instead of hardcoded values
                     .map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
@@ -69,13 +102,7 @@ class _TeacherMatchTheColumnState extends State<TeacherMatchTheColumn> {
               ),
               SizedBox(height: 20), // to add spacing
               ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement the submission logic
-                  // For now, let's print the values to the console
-                  print('Category: $dropdownValue');
-                  print('Question: ${questionController.text}');
-                  print('Answer: ${answerController.text}');
-                },
+                onPressed: submitQuestion, // Call the submitQuestion method when the button is pressed
                 child: Text('Submit Question'),
               ),
             ],
