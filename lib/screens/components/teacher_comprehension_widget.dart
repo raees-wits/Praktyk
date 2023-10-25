159bc48c159d5aa4c9051265ad0109ed617268b4
Oriinga <2433303@students.wits.ac.za>
2023-10-25 17:17:27 +0200

    Added deletion of Comprehensions and FITB sentences

--- lib/screens/components/teacher_comprehension_widget.dart
+++ lib/screens/components/teacher_comprehension_widget.dart
@@ -3,47 +3,69 @@ import 'package:e_learning_app/screens/comprehension_question_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class TeacherComprehensionWidget extends StatelessWidget {
+class TeacherComprehensionWidget extends StatefulWidget {
   String comprehensionTitle = "";
   String comprehensionID = "";
   String comprehensionText = "";
+  Function() onDeleted;
 
   TeacherComprehensionWidget(
       {required this.comprehensionID,
       required this.comprehensionTitle,
-      required this.comprehensionText});
+      required this.comprehensionText,
+      required this.onDeleted});
 
+  @override
+  _TeacherComprehensionWidget createState() => _TeacherComprehensionWidget();
+}
+
+class _TeacherComprehensionWidget extends State<TeacherComprehensionWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10.0),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                TeacherComprehensionQuestionScreen(
-                                  comprehensionID: comprehensionID,
-                                  comprehensionTitle: comprehensionTitle,
-                                  comprehensionText: comprehensionText,
-                                  questionNo: 0,
-                                )));
-                  },
-                  child: Text(
-                    comprehensionTitle,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  )),
-            )
-          ]),
+      child:
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+        TextButton(
+            onPressed: () async {
+              final result = await showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: Text('Delete Comprehension'),
+                        content: Text(
+                            "Are you sure you want to delete this comprehension?"),
+                        actions: [
+                          TextButton(
+                              onPressed: widget.onDeleted,
+                              child: Text("Confirm"))
+                        ],
+                      ));
+            },
+            child: Text('X')),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TeacherComprehensionQuestionScreen(
+                              comprehensionID: widget.comprehensionID,
+                              comprehensionTitle: widget.comprehensionTitle,
+                              comprehensionText: widget.comprehensionText,
+                              questionNo: 0,
+                            )));
+              },
+              child: Text(
+                widget.comprehensionTitle,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              )),
+        )
+      ]),
     );
   }
 }
